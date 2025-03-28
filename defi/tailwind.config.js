17bcc2ee94f4c664558342673fcb76f84c9a6982
aditiisinha <aditisinha0706@gmail.com>
2025-03-28 13:06:15 +0530

    First Commit

---
+++ defi/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",               // Include the index.html file
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript, JSX, TypeScript, and TSX files inside the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
